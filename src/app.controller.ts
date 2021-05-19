1871844199241b4c04019dfc604c86e23f16d8f8
SydneyDC <sydneydeclercq@hotmail.com>
2021-05-19 21:27:10 +0200

    add heroku deployment requirements

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,12 +3,12 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @Header('Content-Type', 'test/html')
-  getHello(): {name:string} {
-    return {name:"Max"}
-    // return this.appService.getHello();
-  }
+   @Get()
+   @Header('Content-Type', 'test/html')
+   getHello(): { name: string } {
+      return { name: 'Max' };
+      // return this.appService.getHello();
+   }
 }
