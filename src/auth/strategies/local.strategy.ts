ff3decad3f59f729894cfaa40891cd1b69e18fed
SydneyDC <sydneydeclercq@hotmail.com>
2021-05-20 23:58:08 +0200

    delete readonly

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -7,7 +7,7 @@ import { User } from '../../users/user.model';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-   constructor(private readonly authService: AuthService) {
+   constructor(private authService: AuthService) {
       super({ usernameField: 'email' });
    }
 
